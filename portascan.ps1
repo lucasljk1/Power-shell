31b1f39be3805d3369a4e0ebba2e7041f1d60205
Lucas de Jesus Matoso <86437350+lucasljk1@users.noreply.github.com>
2024-01-10 22:46:14 -0300

    PortScan-1
    Esse é o primeiro Port Scan que eu fiz para utilizar com o intuito de buscar hosts com porta aberta e fechada e retorna a mensagem de aberto ou fechado.

---
+++ portascan.ps1
@@ -0,0 +1,14 @@
+﻿param ($ip, $porta)
+#Parametros ip e porta são parametros que serão informados
+if (!$ip -or !$porta) {# se o usuario informa somente o ip ou somente a porta irá retorna o modo de uso 
+    echo " PORTA SCANNER "
+    echo " modo de uso | ./portscan.ps1 192.168.0.1 80 ou g1.com.br 80"
+} else {
+if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet) {
+    echo "Porta aberta" 
+    #Caso a porta do host ou do site informado esteja aberta 
+} else {
+    echo "Porta fechada"
+      #Caso a porta do host ou do site informado esteja fechada 
+}
+}
