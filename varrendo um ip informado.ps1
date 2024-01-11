327997ece42ce5fc22f6f6f48634735a242982b6
Lucas de Jesus Matoso <86437350+lucasljk1@users.noreply.github.com>
2024-01-10 22:40:34 -0300

    Verificando inumeros Hosts com uma Porta fixa
    Você já quis saber se um cluster está com uma porta aberta especifica ? esse script pode te ajudar a desvendar isso. Pois o mesmo testa inumeros hosts (dispositivos) em uma porta especifica

---
+++ varrendo um ip informado.ps1
@@ -0,0 +1,6 @@
+﻿#Meu terceiro script
+$ip = Read-Host "Digite o IP "
+#Aguardando o usuario digitar o ip 
+$porta = 80 
+#Porta fixa ou seja sempre vai ser a porta 80 
+echo "Varrendo o Host: $ip e Porta: $porta"
