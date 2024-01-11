2f38e83f4ac8cced6ada8ebbc68d2ca7300ab730
Lucas de Jesus Matoso <86437350+lucasljk1@users.noreply.github.com>
2024-01-10 22:53:47 -0300

    Portscan-2
    Esse porta scan usa o foreach ou seja um laço de repetição para automatizar ao invés de você digitar uma porta de cada vez, você estima o numero de portas e ele faz isso com o host fixo, É muito útil para testar portas abertas em um servidor, Exemplo Eu quero ver se as portas 1 até 1024 do meu servidor está aberto eu informo o ip e o script faz isso por mim

---
+++ portscan2.ps1
@@ -0,0 +1,14 @@
+﻿param ($ip, $porta)
+if (!$ip){
+    echo " PORTA SCANNER "
+    echo " modo de uso | ./portscan2.ps1 192.168.0.1 80 ou g1.com.br 80"
+} else {
+foreach ($porta in 1..1024){
+if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet) {
+    echo "Porta $porta aberta" 
+    
+} else {
+    echo "Porta $porta fechada"
+      
+}}
+}
