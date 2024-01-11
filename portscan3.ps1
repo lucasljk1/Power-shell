25126f6a690968e5d8651fc03b928e09e4a73096
Lucas de Jesus Matoso <86437350+lucasljk1@users.noreply.github.com>
2024-01-10 22:55:43 -0300

    Portscan3
    Porta scan 3 é usando com top portas, função similar ao nmap, porém, pode ser que o dispositivo já reconheça o nmap e o bloqueie. Já o seu script não.

---
+++ portscan3.ps1
@@ -0,0 +1,15 @@
+﻿param ($ip, $porta)
+if (!$ip){
+    echo " PORTA SCANNER "
+    echo " modo de uso | ./portscan2.ps1 192.168.0.1 80 ou g1.com.br 80"
+} else {
+$topports = 21,22,53,80, 443, 3306
+foreach ($porta in 1..1024){
+if (Test-NetConnection $ip -Port $porta -WarningAction SilentlyContinue -InformationLevel Quiet) {
+    echo "Porta $porta aberta" 
+    
+} else {
+    echo "Porta $porta fechada"
+      
+}}
+}
